20c858d6da2d47f55fb637283c02534e294d75ff
GwangMinBack <bgm8862@gmail.com>
2018-05-31 15:44:06 +0900

    Navigation을 정적으로 변경
    - padding을 %에서 px로 변경
    - list가 안 움직이도록 변경
    - 미디어 쿼리 제거

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,34 +1,36 @@
 <template>
   <nav class="nav">
-    <div class="nav__logo">
+    <div class="nav__contants">
+      <div class="nav__contants__logo">
       <router-link to="/">
         <img
-          class="nav__logo--img"
+          class="nav__contants__logo--img"
           src="./../assets/Entry__logo--img.png"
           alt="EntryDSM 이미지 로고입니다."/>
         <img
-          class="nav__logo--text"
+          class="nav__contants__logo--text"
           src="./../assets/Entry__logo--text.png"
           alt="EntryDSM 텍스트 로고입니다."/>
       </router-link>
     </div>
-    <ul class="nav__list">
-      <li class="nav__list__link">
+    <ul class="nav__contants__list">
+      <li class="nav__contants__list__link">
         <router-link to="/explan">시스템 소개</router-link>
       </li>
-      <li class="nav__list__link">
+      <li class="nav__contants__list__link">
         <router-link to="/write">원서작성</router-link>
       </li>
-      <li class="nav__list__link">
+      <li class="nav__contants__list__link">
         <router-link to="/summary">전형요강</router-link>
       </li>
-      <li class="nav__list__link">
+      <li class="nav__contants__list__link">
         <router-link to="/schedule">모집일정</router-link>
       </li>
-      <li class="nav__list__link nav__list__link--login">
+      <li class="nav__contants__list__link nav__contants__list__link--login">
         로그인
       </li>
     </ul>
+    </div>
   </nav>
 </template>
 
@@ -53,32 +55,37 @@ export default {
 .nav {
   height: 60px;
   box-shadow: 0 2px 10px 0 rgba(99, 141, 147, 0.05);
-  display: flex;
-  align-items: center;
-  padding: 0 15% 0 15%;
-  @include e(logo){
+  padding: 0 250px;
+  @include e(contants){
+    position: relative;
+    width: 1140px;
+    height: 100%;
     display: flex;
-    justify-content: center;
     align-items: center;
-    cursor: pointer;
-    @include m(img) {
-      height: 30px;
-    }
-    @include m(text){
-      position: relative;
-      left: 11px;
-      width: 69px;
-    }
-  }
-  @include e(list) {
-    position: absolute;
-    right: 15%;
-    @include e(link){
-      margin-left: 45px;
+    @include e(logo){
+      display: flex;
+      justify-content: center;
+      align-items: center;
       cursor: pointer;
-      display: inline-block;
-      @include m(login){
-        margin-left: 103px;
+      @include m(img) {
+        height: 30px;
+      }
+      @include m(text){
+        position: relative;
+        left: 11px;
+        width: 69px;
+      }
+    }
+    @include e(list) {
+      position: absolute;
+      right: 0;
+      @include e(link){
+        margin-left: 45px;
+        cursor: pointer;
+        display: inline-block;
+        @include m(login){
+          margin-left: 103px;
+        }
       }
     }
   }
@@ -91,21 +98,6 @@ a {
   &:visited{
     color: #000;
     text-decoration: none;
-  }
-}
-
-@media (max-width: 990px) {
-  .nav {
-    @include e(list) {
-      @include e(link){
-        font-size: 12px;
-        margin-left: 10px;
-        cursor: pointer;
-        @include m(login){
-          margin-left: 40px;
-        }
-      }
-    }
   }
 }
 </style>
