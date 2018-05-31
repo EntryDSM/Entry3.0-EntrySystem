062c2161dbf9e24fa880e9e85cee84ddec75b4d1
GwangMinBack <bgm8862@gmail.com>
2018-05-31 19:18:34 +0900

    Navigation 주석처리

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="app">
-    <Navi />
     <router-view />
   </div>
 </template>
 
 <script>
-import Navi from './components/Navigation';
+// import Navi from './components/Navigation';
 // import EntryFooter from '@/components/EntryFooter';
 // import Headline from '@/components/Headline';
 
 export default {
   name: 'app',
-  components: {
-    Navi,
-  },
 };
 </script>
 
