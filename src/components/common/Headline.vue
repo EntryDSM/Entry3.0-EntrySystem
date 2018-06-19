14f43ba1f2c8d5457f4e53017e258740da799f72
Junmo <yunedoopea610@gmail.com>
2018-06-19 13:21:06 +0900

    헤드라인 props 추가, 스타일 조금 수정

--- src/components/common/Headline.vue
+++ src/components/common/Headline.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="headline">
-    <h4 class="headline__school-information">{{ schoolName }}</h4>
-    <h1 class="headline__title">
-      {{ title }}
-    </h1>
+    <h4 class="headline__sub-text">{{ subText }}</h4>
+    <h1 class="headline__title">{{ title }}</h1>
   </div>
 </template>
 
@@ -11,15 +9,14 @@
 export default {
   name: 'Headline',
   props: {
+    subText: {
+      type: String,
+      default: '대덕소프트웨어마이스터고등학교',
+    },
     title: {
       type: String,
       required: true,
     },
-  },
-  data() {
-    return {
-      schoolName: '대덕소프트웨어마이스터고등학교',
-    };
   },
 };
 </script>
@@ -33,13 +30,12 @@ export default {
   margin: 0 auto;
   text-align: left;
   height: 260px;
-  @include e('school-information') {
+  @include e('sub-text') {
     font-weight: 500;
     font-size: 20px;
     height: 21px;
     line-height: 21px;
     color: #000000;
-    letter-spacing: 0.5px;
   }
   @include e('title') {
     font-weight: 500;
@@ -50,7 +46,11 @@ export default {
     padding: 10px 0 15px;
     border-bottom: 2.5px solid #70b1ba;
     box-sizing: border-box;
-    width: 350px;
+    letter-spacing: -2px;
+    padding: {
+      right: 50px;
+    }
+    width: fit-content;
   }
 }
 </style>
