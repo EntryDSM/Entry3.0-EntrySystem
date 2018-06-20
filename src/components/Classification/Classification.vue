1e3016a0473cea31f0c79192b06e85626c38e910
Junmo <yunedoopea610@gmail.com>
2018-06-20 15:58:55 +0900

    첫번째 줄 스타일 추가

--- src/components/Classification/Classification.vue
+++ src/components/Classification/Classification.vue
@@ -8,9 +8,10 @@
           <div class="classification__cover__form__colums__name">
             검정고시 지원여부
           </div>
-          <div class="classification__cover__form__colums">
-            <input type="checkbox" class="input-checkbox">
-            <span class="classification__cover__form__colums__sign">
+          <div class="classification__cover__form__colums__input-content">
+            <input type="checkbox" id="input-checkbox">
+            <label class="input-checkbox" for="input-checkbox"></label>
+            <span class="classification__cover__form__colums__input-content__sign">
               * 검정고시를 통하여 지원하실 경우 체크해주세요
             </span>
           </div>
@@ -56,11 +57,51 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../style/setting';
+$color-main1: #769b9f;
+$color-main2: #5f8a90;
+$color-main3: #608a8e;
 
 .classification {
   @include e('cover') {
     width: 1140px;
     margin: 0 auto;
+    @include e('form') {
+      border: {
+        top: solid 1px $color-main1;
+        bottom: solid 1px $color-main1;
+      }
+      @include e('colums') {
+        height: 75px;
+        line-height: 75px;
+        font-size: 0;
+        border {
+          bottom: solid 1px $color-main2;
+        }
+        @include e('name') {
+          font-size: 20px;
+          height: 100%;
+          width: 15%;
+          text-align: center;
+          display: inline-block;
+        }
+        @include e('input-content') {
+          font-size: 18px;
+          padding-left: 30px;
+          box-sizing: border-box;
+          width: 85%;
+          height: 100%;
+          display: inline-block;
+          position: relative;
+          @include e('sign') {
+            position: absolute;
+            right: 0;
+            top: 0;
+            font-size: 14px;
+            color: $color-main3;
+          }
+        }
+      }
+    }
   }
 }
 </style>
