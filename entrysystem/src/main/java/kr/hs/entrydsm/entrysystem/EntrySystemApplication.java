6cdf7c6c2339831de04da32c30bfeb9344de945c
Terry Yun <nooheat1228@gmail.com>
2018-06-21 09:10:10 +0900

    backend project active profiles setting

--- entrysystem/src/main/java/kr/hs/entrydsm/entrysystem/EntrySystemApplication.java
+++ entrysystem/src/main/java/kr/hs/entrydsm/entrysystem/EntrySystemApplication.java
@@ -1,12 +1,17 @@
 package kr.hs.entrydsm.entrysystem;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EntrySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EntrySystemApplication.class, args);
-	}
+    private static final String APPLICATION_YML_LOCATIONS = "spring.config.location=" +
+            "classpath:application.yml," +
+            "/app/config/entrysystem/application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EntrySystemApplication.class)
+                .properties(APPLICATION_YML_LOCATIONS).run(args);
+    }
 }
