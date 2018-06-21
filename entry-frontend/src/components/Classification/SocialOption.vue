5ee7fd9f91034be4435ab8dd89bb0988639d83c9
Junmo <yunedoopea610@gmail.com>
2018-06-21 16:43:56 +0900

    Socail option 컴포넌트 기능 추가(미완)

--- entry-frontend/src/components/Classification/SocialOption.vue
+++ entry-frontend/src/components/Classification/SocialOption.vue
@@ -1,10 +1,11 @@
 <template>
   <ul class="social-option">
-    <li class="social-option__item">
-    </li>
-    <li class="social-option__item social-option__item--focused">
-    </li>
-    <li class="social-option__item social-option__item--hover">
+    <li class="social-option__item"
+      v-for="(option, index) in options"
+      :key="index"
+      :class="{'social-option__item--focused': selected == option.value}"
+      @click="select(option.value)">
+      {{ option.text }}
     </li>
   </ul>
 </template>
@@ -12,6 +13,22 @@
 <script>
 export default {
   name: 'social-option',
+  props: {
+    options: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    select(value) {
+      this.selected = value;
+      this.$emit('input', value);
+    },
+  },
 };
 </script>
 
@@ -19,19 +36,52 @@ export default {
 @import '../../style/setting';
 
 .social-option {
+  position: absolute;
+  z-index: 2;
+  top: 60px;
+  left: 0;
   width: 250px;
-  height: 279px;
   background-color: #ffffff;
   border: solid 1px #7b9fa2;
   @include e('item') {
     width: 250px;
     height: 40px;
     box-sizing: border-box;
+    font-size: 16px;
+    line-height: 40px;
+    position: relative;
     border: {
       bottom: solid 1px #9bb6b9;
     }
-    @include m('hover') {
+    padding: {
+      left: 20px;
+    }
+    &::after {
+      font-size: 18px;
+      position: absolute;
+      right: 20px;
+      color: #709ea2;
+    }
+    &:hover {
+      background-color: #d0e5ea;
+      &::after {
+        content: '〉';
+      }
+    }
+    &:last-child {
+      border: none;
+    }
+    @include m('focused') {
       background-color: #f7fbfc;
+      &::after {
+        content: '✓';
+      }
+      &:hover {
+        background-color: #f7fbfc;
+        &::after {
+          content: '✓';
+        }
+      }
     }
   }
 }
