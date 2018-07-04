8db36f20772241d7244e0caa17465b92efe121b2
Junmo <yunedoopea610@gmail.com>
2018-07-04 12:24:36 +0900

    입력란 마크업 추가, 셀렉트 박스 데이터 마크업 추가

--- entry-frontend/src/components/PersonalInformation/PersonalInformation.vue
+++ entry-frontend/src/components/PersonalInformation/PersonalInformation.vue
@@ -8,8 +8,152 @@
       <div class="form__cover__form">
         <div class="form__cover__form__colums">
           <div class="form__cover__form__colums__name">
+            이름
           </div>
           <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-name" v-model="name">
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            성별
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="radio"
+              class="input-radio"
+              id="female"
+              value="female"
+              @click="isOpen = false"
+              v-model="sex">
+            <label class="input-radio-label" for="female">
+              <span class="input-radio-span"></span>
+            </label>
+            <label class="form__cover__form__colums__input-content__label"
+              for="female">
+              여자
+            </label>
+
+            <input type="radio"
+              class="input-radio"
+              id="male"
+              value="male"
+              @click="isOpen = false"
+              v-model="sex">
+            <label class="input-radio-label" for="male">
+              <span class="input-radio-span"></span>
+            </label>
+            <label class="form__cover__form__colums__input-content__label"
+              for="male">
+              남자
+            </label>
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            생년월일
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <selectbox class="selectbox-year"
+              v-model="year"
+              :options="yearOptions"/>
+            <label class="title--input">
+              년
+            </label>
+            <selectbox class="selectbox-month"
+              v-model="month"
+              :options="monthOptions"/>
+            <label class="title--input">
+              월
+            </label>
+            <selectbox class="selectbox-day"
+              v-model="day"
+              :options="dayOptions"/>
+            <label class="title--input">
+              일
+            </label>
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            학번
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-school-grade" v-model="schoolGrade">
+            <label class="title--input">
+              학년
+            </label>
+            <input type="text" class="input-text input-text-school-class" v-model="schoolClass">
+            <label class="title--input">
+              반
+            </label>
+            <input type="text" class="input-text input-text-school-number" v-model="schoolnumber">
+            <label class="title--input">
+              번
+            </label>
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            중학교명
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-middle-school" v-model="middleSchool">
+            <button class="button button-search">검색</button>
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            보호자명
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-guardian-name" v-model="guardianName">
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            학교 연락처
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-school-contact" v-model="schoolContact">
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            보호자 연락처
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text"
+              class="input-text input-text-guardian-contact"
+              v-model="guardianContact">
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            본인 연락처
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text" class="input-text input-text-contact" v-model="contact">
+          </div>
+        </div>
+        <div class="form__cover__form__colums">
+          <div class="form__cover__form__colums__name">
+            주소
+          </div>
+          <div class="form__cover__form__colums__input-content">
+            <input type="text"
+              class="input-text input-text-zip"
+              v-model="zip"
+              placeholder="우편번호">
+            <input type="text"
+              class="input-text input-text-address"
+              v-model="zip"
+              placeholder="기본주소">
+            <button class="button button-search-address">주소 검색</button>
+            <br>
+            <input type="text"
+              class="input-text input-text-detailed-address"
+              v-model="detailedAddress"
+              placeholder="상세주소">
           </div>
         </div>
       </div>
@@ -33,7 +177,7 @@ import EntryFooter from '../common/EntryFooter';
 import Selectbox from '../common/Selectbox';
 
 export default {
-  name: 'classification',
+  name: 'personalInformation',
   components: {
     Navigation,
     Headline,
@@ -45,7 +189,66 @@ export default {
     return {
       title: '인적 사항',
       subText: '2019 입학원서 작성',
+      yearOptions: [],
+      monthOptions: [],
+      year: 0,
+      month: 0,
+      day: 0,
     };
+  },
+  computed: {
+    dayOptions() {
+      let count;
+      const year = this.year;
+      // 윤년
+      const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+      switch (this.month) {
+        case 2: count = isLeapYear ? 29 : 28; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: count = 30; break;
+        default: count = 31; break;
+      }
+
+      const dayArray = [];
+      for (let i = 1; i <= count; i += 1) {
+        const pad = i >= 10 ? '' : '0';
+        const day = pad + i;
+
+        dayArray[i - 1] = {
+          text: day,
+          value: i,
+        };
+      }
+      return dayArray;
+    },
+  },
+  created() {
+    // year options
+    const lastYear = 2005;
+    const count = 20;
+    const yearArray = [];
+    for (let i = 0; i < count; i += 1) {
+      const year = lastYear - i;
+      yearArray[i] = {
+        text: `${year}`,
+        value: year,
+      };
+    }
+    this.yearOptions = yearArray;
+
+    // month options
+    const monthArray = [];
+    for (let i = 1; i <= 12; i += 1) {
+      const pad = i >= 10 ? '' : '0';
+      monthArray[i - 1] = {
+        text: pad + i,
+        value: i,
+      };
+    }
+    this.monthOptions = monthArray;
   },
   methods: {
     movePrev() {
@@ -60,65 +263,65 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../style/setting';
-$color-main1: #769b9f;
-$color-main2: #eff3f4;
-$color-main3: #608a8e;
 
-.classification {
-  @include e('cover') {
-    width: 1140px;
-    margin: 0 auto;
-    @include e('form') {
-      border: {
-        top: solid 1px $color-main1;
-        bottom: solid 1px $color-main1;
-      }
-      @include e('colums') {
-        height: 75px;
-        line-height: 75px;
-        font-size: 0;
-        border: {
-          bottom: solid 1px $color-main2;
-        }
-        @include e('name') {
-          font-size: 20px;
-          height: 100%;
-          width: 15%;
-          text-align: center;
-          display: inline-block;
-        }
-        @include e('input-content') {
-          font-size: 0px;
-          padding-left: 30px;
-          box-sizing: border-box;
-          width: 85%;
-          height: 100%;
-          display: inline-block;
-          position: relative;
-          user-select: none;
-          @include e('sign') {
-            position: absolute;
-            right: 0;
-            top: 0;
-            font-size: 14px;
-            color: $color-main3;
-          }
-          @include e('label') {
-            font-size: 18px;
-            display: inline-block;
-            min-width: 100px;
-            padding: {
-              left: 10px;
-              right: 50px;
-            }
-          }
-        }
-      }
+.button {
+  height: 40px;
+  border-radius: 5px;
+  background-color: #f7fbfc;
+  border: solid 1px #5f8a90;
+  font-size: 18px;
+  color: #296169;
+  margin-left: 15px;
+  &-search {
+    width: 75px;
+  }
+  &-search-address {
+    width: 125px;
+  }
+}
+
+.input-text {
+  &-name, &-guardian-name, &-zip {
+    width: 150px;
+  }
+  &-middle-school, &-school-contact, &-guardian-contact, &-contact {
+    width: 250px;
+  }
+  &-address {
+    width: 275px;
+    margin: {
+      left: 15px;
+    }
+  }
+  &-detailed-address {
+    width: 580px;
+    margin-top: 20px;
+  }
+  &-school {
+    &-grade, &-class, &-number {
+      width: 50px;
+      text-align: center;
+    }
+  }
+}
+
+.title {
+  @include m('input') {
+    font-size: 18px;
+    width: fit-content;
+    margin: {
+      left: 10px;
+      right: 35px;
     }
   }
 }
 
 .selectbox {
-  width: 150px;
+  &-year {
+    width: 125px;
+  }
+  &-month, &-day {
+    width: 75px;
+  }
 }
 </style>
