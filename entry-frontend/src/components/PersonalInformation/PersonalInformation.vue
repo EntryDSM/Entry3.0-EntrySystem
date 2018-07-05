698305e368fe8a21000dae6cd6ca98c1b957dec4
Junmo <yunedoopea610@gmail.com>
2018-07-05 19:19:00 +0900

    우편번호 검색 API 적용, 데이터바인딩 완료

--- entry-frontend/src/components/PersonalInformation/PersonalInformation.vue
+++ entry-frontend/src/components/PersonalInformation/PersonalInformation.vue
@@ -184,7 +184,9 @@
               placeholder="기본주소"
               :value="address"
               @input="address = $event.target.value">
-            <button class="button button-search-address">주소 검색</button>
+            <button class="button button-search-address" @click="openSearchAdress">
+              주소 검색
+            </button>
             <br>
             <input type="text"
               class="input-text input-text-detailed-address"
@@ -304,6 +306,33 @@ export default {
       // 숫자만 입력 가능
       e.target.value = e.target.value.replace(/[^\d]/g, '');
     },
+    openSearchAdress() {
+      const vueObject = this;
+      new window.daum.Postcode({
+        oncomplete(data) {
+          let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+          let extraRoadAddr = ''; // 도로명 조합형 주소 변수
+
+          // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+          // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+          if (data.bname && /[동|로|가]$/g.test(data.bname)) {
+            extraRoadAddr += data.bname;
+          }
+          // 건물명이 있고, 공동주택일 경우 추가한다.
+          if (data.buildingName && data.apartment === 'Y') {
+            extraRoadAddr += (extraRoadAddr !== '' ? `, ${data.buildingName}` : data.buildingName);
+          }
+          // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+          extraRoadAddr = extraRoadAddr ? ` (${extraRoadAddr})` : '';
+          // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+          fullRoadAddr += fullRoadAddr ? extraRoadAddr : '';
+
+          // 우편번호와 주소 정보를 해당 필드에 넣는다.
+          vueObject.zip = data.zonecode; // 5자리 새 우편번호 사용
+          vueObject.address = fullRoadAddr;
+        },
+      }).open();
+    },
     movePrev() {
       this.$router.push('/');
     },
@@ -325,6 +354,10 @@ export default {
   font-size: 18px;
   color: #296169;
   margin-left: 15px;
+  outline: none;
+  &:active {
+    background-color: #D8E6E9;
+  }
   &-search {
     width: 75px;
   }
