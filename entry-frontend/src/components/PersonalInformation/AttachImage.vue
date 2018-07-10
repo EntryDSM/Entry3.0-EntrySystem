b6a112d1bc8f72bb67be0d21c5810fc7dd820ab7
Junmo <yunedoopea610@gmail.com>
2018-07-10 10:29:46 +0900

    이미지 첨부 컴포넌트 스타일 마크업

--- entry-frontend/src/components/PersonalInformation/AttachImage.vue
+++ entry-frontend/src/components/PersonalInformation/AttachImage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="attach-image">
-    <input type="file"/>
+    <label class="attach-image__label">
+    </label>
   </div>
 </template>
 
@@ -10,6 +11,32 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../style/setting';
+
+.attach-image {
+  width: 315px;
+  height: 375px;
+  background-color: #fff;
+  box-sizing: border-box;
+  border: {
+    left: 1px solid #eff3f4;
+    bottom: 1px solid #eff3f4;
+  }
+  padding: {
+    left: 17px;
+    right: 5px;
+    top: 20px;
+    bottom: 15px;
+  }
+  @include e('label') {
+    width: 100%;
+    height: 100%;
+    display: block;
+    box-sizing: border-box;
+    background-color: #f7fbfc;
+    border: solid 0.5px #8aaaad;
+  }
+}
 
 </style>
