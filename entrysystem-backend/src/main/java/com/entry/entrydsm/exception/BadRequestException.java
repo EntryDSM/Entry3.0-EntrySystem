fd56f6f036ea6670b77444b240300a5ab35f7a31
syeutyu <syeuty@naver.com>
2018-07-25 14:43:11 +0900

    Intercepotor Folder -> exception Folder

---
+++ entrysystem-backend/src/main/java/com/entry/entrydsm/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package com.entry.entrydsm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
