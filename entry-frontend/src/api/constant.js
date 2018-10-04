c1d966377d6ccec7cc2942291ccd43e4b9d88e9e
JungYunJae <jung22337@naver.com>
2018-10-04 20:14:33 +0900

    URL 임시 변경 (#128)

--- entry-frontend/src/api/constant.js
+++ entry-frontend/src/api/constant.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URI: 'http://114.108.135.15/api/me/',
-  SCHOOL_URI: 'http://114.108.135.15/api/schools/',
+  BASE_URI: 'http://entry.entrydsm.hs.kr/api/me/',
+  SCHOOL_URI: 'http://entry.entrydsm.hs.kr/api/schools/',
 };
