bad4060b833e7b5ddc61ecace13373121a4f78f2
SayBGM <bgm537@saybgm.me>
2018-10-12 19:53:25 +0900

    크롬 이외 브라우저일시 인증이 완료되며 크롬환경에서 다시 접속해 달라는 메세지를 띄움

--- entry-frontend/src/components/Authorization/SignupConfirm.vue
+++ entry-frontend/src/components/Authorization/SignupConfirm.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+/* eslint-disable */
 import contact from './../../api/contact';
 
 const emailReg = /^([0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*)(@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3})$/;
@@ -12,8 +13,14 @@ export default {
   ],
   created() {
     const { s, e } = this.$toastr;
+    const isChrome = !!window.chrome && !!window.chrome.webstore;
     contact.confirmSignup(this.code).then((res) => {
         const { token, email } = res.data.data.accessToken;
+        if (!isChrome) {
+          alert('인증이 완료되었습니다. 크롬환경에서 다시 접속해주세요 :)');
+          window.location.href = 'https://www.google.com/intl/ko_ALL/chrome/';
+          return;
+        }
         this.$cookies.set('accessToken', token, '4d');
         this.$store.dispatch('getClassify', { token });
         this.$store.dispatch('getInfo', { token });
