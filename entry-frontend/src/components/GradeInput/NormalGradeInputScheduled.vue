941772bee4751996fa8f9e6eed662402baa17e27
SayBGM <bgm537@saybgm.me>
2018-10-16 19:35:07 +0900

    성적입력에서 this.$router.go가 2번 실행되는 버그 수정

--- entry-frontend/src/components/GradeInput/NormalGradeInputScheduled.vue
+++ entry-frontend/src/components/GradeInput/NormalGradeInputScheduled.vue
@@ -1359,21 +1359,6 @@ export default {
       },
     },
   },
-  created() {
-    const token = this.$cookies.get('accessToken');
-    const { e } = this.$toastr;
-    if (token === undefined || token === null || token === '') {
-      this.$router.go(-1);
-      e('해당 페이지는 로그인이 필요합니다.');
-      this.$store.commit('changeIndex', {
-        index: 1,
-      });
-    }
-    if (this.$store.state.mypage.applyStatus.finalSubmit) {
-      e('최종 제출 후에는 접근 할 수 없습니다.');
-      this.$router.go(-1);
-    }
-  },
   methods: {
     // 봉사 및 출석 Commit - 완료
     updateVolunteerNAttendance(field, value) {
