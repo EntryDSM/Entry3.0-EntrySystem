caccc583b46e0485880bc8ef6b4bf35742a7bc45
SayBGM <bgm537@saybgm.me>
2018-10-16 22:19:01 +0900

    Preview WaterMack 변경

--- entry-frontend/src/components/Preview/Application.vue
+++ entry-frontend/src/components/Preview/Application.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="preview-container">
-    <div class="watermark" v-if="!finalSubmit">EntryDSM 3.0 미리보기</div>
-    <div id="application-header">
+    <div class="watermark" v-if="!finalSubmit">
+      <div class="watermark__wrong">
+        * 본 원서(테스트 원서)를 출력하여 실제 접수시 사용시 정상적으로 지원이 완료되지 않을 수 있습니다.
+      </div>
+      <div class="watermark__contents">
+        <img class="watermark__contents__img" src="../../assets/entry_logo.png">
+        본 원서는 Entry3.0에서 제공하는<br/>
+        미리보기 테스트 원서입니다.
+      </div>
+      <div class="watermark__wrong watermark__wrong--last">
+        * 본 원서(테스트 원서)를 출력하여 실제 접수시 사용시 정상적으로 지원이 완료되지 않을 수 있습니다.
+      </div>
+    </div>
+    <div class="container--wrapper">
+      <div id="application-header">
       2019학년도 대덕소프트웨어마이스터고등학교 입학원서
     </div>
     <span id="application-info-tables">
@@ -189,6 +202,7 @@
       <p>지원자 성명 <span class="application-info-long-blank">{{personName}}</span>(인)</p>
       <p>보호자 성명 <span class="application-info-long-blank">{{parentName}}</span>(인)</p>
       <p>대덕소프트웨어마이스터고등학교장 귀하</p>
+    </div>
     </div>
   </div>
 </template>
@@ -445,13 +459,43 @@ input[type="checkbox"] {
 }
 .watermark {
   position: absolute;
-  top: 400px;
-  left: 60px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  z-index: 1;
+  text-align: center;
   opacity: 0.2;
-  font-family: sans-serif;
-  font-size: 50px;
-  color: #000;
-  transform: rotate(-30deg);
 }
 
+.watermark__wrong {
+  font-size: 13px;
+  color: #F00;
+  font-weight: 900;
+}
+.watermark__wrong--last{
+  position: absolute;
+  bottom: 20px;
+}
+.watermark__contents{
+  position: absolute;
+  margin: 0 auto;
+  top: calc(50% - 75px);
+  left: 145px;
+  font-size: 20px;
+  font-weight: 900;
+}
+.watermark__contents__img{
+  width: 300px;
+  -webkit-filter: grayscale(100%);
+  filter: gray;
+  margin: 0 auto;
+  display: block;
+  margin-bottom: 20px;
+}
+.container--wrapper {
+  position: relative;
+  z-index: 3;
+}
 </style>
--- entry-frontend/src/components/Preview/Introduction.vue
+++ entry-frontend/src/components/Preview/Introduction.vue
@@ -1,34 +1,48 @@
 <template>
   <div id="preview-container">
-    <div class="watermark" v-if="!finalSubmit">EntryDSM 3.0 미리보기</div>
-    <div id="introduction-header">
-      자기소개서
+    <div class="watermark" v-if="!finalSubmit">
+      <div class="watermark__wrong">
+        * 본 원서(테스트 원서)를 출력하여 실제 접수시 사용시 정상적으로 지원이 완료되지 않을 수 있습니다.
+      </div>
+      <div class="watermark__contents">
+        <img class="watermark__contents__img" src="../../assets/entry_logo.png">
+        본 원서는 Entry3.0에서 제공하는<br/>
+        미리보기 테스트 원서입니다.
+      </div>
+      <div class="watermark__wrong watermark__wrong--last">
+        * 본 원서(테스트 원서)를 출력하여 실제 접수시 사용시 정상적으로 지원이 완료되지 않을 수 있습니다.
+      </div>
     </div>
-    <pre id="introduction-title"> 【 지원자 기재사항 】</pre>
-    <span id="introduction-info-tables">
-      <table>
-        <tbody>
-          <tr>
-            <td><pre style="font-size:12px;">성       명</pre></td>
-            <td style="font-size:12px;">{{personName}}</td>
-          </tr>
-          <tr>
-            <td>출신중학교</td>
-            <td style="font-size:12px;">{{school.name}}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <tbody>
-          <tr>
-            <td><div id="tag-icon">◎</div> <span id="introduction-info-main-text">자기소개서</span> 내용은 특별한 형식은 없으며 개인의 특성 및 성장 과정, 취미·특기, 학교생활, 가족 안에서의 역할, 남들보다 뛰어나다고 생각하는 자신의 장점(특성 혹은 능력)과 보완·발전시켜야 할 단점에대하여 기술하십시오.</td>
-          </tr>
-          <tr>
-            <td><pre id="preview-text">{{introduce}}</pre></td>
-          </tr>
-        </tbody>
-      </table>
-    </span>
+    <div class="container--wrapper">
+      <div id="introduction-header">
+        자기소개서
+      </div>
+      <pre id="introduction-title"> 【 지원자 기재사항 】</pre>
+      <span id="introduction-info-tables">
+        <table>
+          <tbody>
+            <tr>
+              <td><pre style="font-size:12px;">성       명</pre></td>
+              <td style="font-size:12px;">{{personName}}</td>
+            </tr>
+            <tr>
+              <td>출신중학교</td>
+              <td style="font-size:12px;">{{school.name}}</td>
+            </tr>
+          </tbody>
+        </table>
+        <table>
+          <tbody>
+            <tr>
+              <td><div id="tag-icon">◎</div> <span id="introduction-info-main-text">자기소개서</span> 내용은 특별한 형식은 없으며 개인의 특성 및 성장 과정, 취미·특기, 학교생활, 가족 안에서의 역할, 남들보다 뛰어나다고 생각하는 자신의 장점(특성 혹은 능력)과 보완·발전시켜야 할 단점에대하여 기술하십시오.</td>
+            </tr>
+            <tr>
+              <td><pre id="preview-text">{{introduce}}</pre></td>
+            </tr>
+          </tbody>
+        </table>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -132,12 +146,43 @@ td {
 #introduction-info-tables table:nth-child(2) tr:nth-child(2) td:nth-child(1) { text-align: left; padding: 8px; height: 570px; border-top: 0; width: 100%; }
 .watermark {
   position: absolute;
-  top: 400px;
-  left: 60px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  z-index: 1;
+  text-align: center;
   opacity: 0.2;
-  font-family: sans-serif;
-  font-size: 50px;
-  color: #000;
-  transform: rotate(-30deg);
+}
+
+.watermark__wrong {
+  font-size: 13px;
+  color: #F00;
+  font-weight: 900;
+}
+.watermark__wrong--last{
+  position: absolute;
+  bottom: 20px;
+}
+.watermark__contents{
+  position: absolute;
+  margin: 0 auto;
+  top: calc(50% - 75px);
+  left: 145px;
+  font-size: 20px;
+  font-weight: 900;
+}
+.watermark__contents__img{
+  width: 300px;
+  -webkit-filter: grayscale(100%);
+  filter: gray;
+  margin: 0 auto;
+  display: block;
+  margin-bottom: 20px;
+}
+.container--wrapper {
+  position: relative;
+  z-index: 3;
 }
 </style>
